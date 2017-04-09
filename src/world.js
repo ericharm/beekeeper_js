05a5b7242c4675f7d6a56a194fcbf1ec04af7954
ericharm <ericharm@gmail.com>
2017-04-09 13:37:24 -0400

    stub out some data for world class

--- src/world.js
+++ src/world.js
@@ -1,6 +1,15 @@
+var SceneNode = require("./scene_node.js");
+
 module.exports = {
 
-    new: function (canvas) {
+    new: function (context) {
+
+        // private data
+        var canvas = context.canvas;
+        var textures = context.textures;
+        var player = context.player;
+        var sceneGraph = SceneNode.new();
+
 
         // private methods
         function loadTextures() {
